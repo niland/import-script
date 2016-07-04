6af96933128b3aa3f58792ef12b37fee8389c299
martin-laurent <martin.laurent@niland.io>
2016-07-04 16:41:34 +0200

    parallelized version. Allows to begin from a specific line of the csv file

--- niland_importer.py
+++ niland_importer.py
@@ -5,51 +5,63 @@ from nilandapi.client import Client
 import urllib
 import csv
 import argparse
+from joblib import Parallel, delayed
+
+headers = {
+    'reference': 0,
+    'title': 1,
+    'artist_name': 2,
+    'album_name': 3,
+    'popularity': 4,
+    'duration': 5,
+    'isrc': 6,
+    'year': 7,
+    'tags': 8,
+    'audio_path': 9,
+    'audio_url': 10
+}
+
+
+def process_one(row):
+    try:
+        if len(headers) != len(row):
+            raise Exception('Malformed row. Expected %s field, having %s.' % (len(headers), len(row)))
+        try:
+            requ = 'tracks/reference/%s' % row[headers['reference']]
+            importer._client.get(requ)
+            print 'Reference %s already saved.' % (row[headers['reference']])
+        except:
+            tag_ids = importer._handle_tags(row[headers['tags']])
+            track = {
+                'reference': row[headers['reference']],
+                'title': row[headers['title']],
+                'artist': row[headers['artist_name']],
+                'album': row[headers['album_name']],
+                'popularity': row[headers['popularity']],
+                'duration': row[headers['duration']],
+                'isrc': row[headers['isrc']],
+                'year': row[headers['year']],
+                'tags': tag_ids
+            }
+            if 0 != len(row[headers['audio_path']]):
+                track['audio'] = open(row[headers['audio_path']], 'rb')
+            if 0 != len(row[headers['audio_url']]):
+                track['audio'] = open(importer._download_file(row[headers['audio_url']]), 'rb')
+            track = importer._client.post('tracks', track)
+            print 'Reference %s saved Niland ID %s' % (row[headers['reference']], track['id'])
+    except Exception as e:
+        print 'Error for "%s": %s' % (row[headers['reference']], e)
+
 
 class NilandImporter(object):
-    def __init__(self, api_key, csv_path):
+    def __init__(self, api_key, csv_path, start_line=0):
         self._client = Client(api_key)
-        self._reader = csv.reader(open(csv_path, 'rb'), delimiter=';')
+        with open(csv_path, 'rb') as csvfile:
+            remaining_lines = list(csvfile)[start_line:]
+        self._reader = csv.reader(remaining_lines, delimiter=';')
 
-    def process(self):
-        headers = {
-            'reference': 0,
-            'title': 1,
-            'artist_name': 2,
-            'album_name': 3,
-            'popularity': 4,
-            'duration': 5,
-            'isrc': 6,
-            'year': 7,
-            'tags': 8,
-            'audio_path': 9,
-            'audio_url': 10
-        }
-
-        for row in self._reader:
-            try:
-                if len(headers) != len(row):
-                    raise Exception('Malformed row. Expected %s field, having %s.' % (len(headers), len(row)))
-                tag_ids = self._handle_tags(row[headers['tags']])
-                track = {
-                    'reference': row[headers['reference']],
-                    'title': row[headers['title']],
-                    'artist': row[headers['artist_name']],
-                    'album': row[headers['album_name']],
-                    'popularity': row[headers['popularity']],
-                    'duration': row[headers['duration']],
-                    'isrc': row[headers['isrc']],
-                    'year': row[headers['year']],
-                    'tags': tag_ids
-                }
-                if 0 != len(row[headers['audio_path']]):
-                    track['audio'] = open(row[headers['audio_path']], 'rb')
-                if 0 != len(row[headers['audio_url']]):
-                    track['audio'] = open(self._download_file(row[headers['audio_url']]), 'rb')
-                track = self._client.post('tracks', track)
-                print 'Line %s saved Niland ID %s' % (self._reader.line_num, track['id'])
-            except Exception as e:
-                print 'Line %s error "%s"' % (self._reader.line_num, e)
+    def process(self, njobs):
+        Parallel(n_jobs=njobs, verbose=11)(delayed(process_one)(row) for row in self._reader)
 
     def _handle_tags(self, value):
         tag_ids = []
@@ -88,7 +100,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Niland Catalog Importer')
     parser.add_argument('--api-key', help='Your API key', required=True)
     parser.add_argument('--csv-path', help='Full path to the csv file', required=True)
+    parser.add_argument('--njobs', help='Number of jobs to throw', required=True)
+    parser.add_argument('--start-line', help='Line from which to start uploading', required=False, type=int)
     args = parser.parse_args()
 
-    importer = NilandImporter(args.api_key, args.csv_path)
-    importer.process()
+    global importer
+    importer = NilandImporter(args.api_key, args.csv_path, args.start_line)
+    importer.process(int(args.njobs))
