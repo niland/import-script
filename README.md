7e8c57b1252de1b0fa7d7fb694a5bb76d5d4ba3f
martin-laurent <martin.laurent@niland.io>
2016-07-11 17:55:16 +0200

    Break column dependance. Update readme, samples and requirements

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Niland Import Script
 
+Installation
+----
+```bash
+git clone https://github.com/niland/import-script.git
+cd import-script
+pip install -r requirements.txt
+```
+
 Usage
 ----
 ```bash
@@ -8,31 +16,28 @@ niland_importer.py --api-key YOUR_PERSONAL_API_KEY --csv-path FULL_PATH_TO_THE_C
 
 CSV Specifications
 ----
-### Rules ###
-* The csv file must contain 11 columns per line. Leave the column blank if you have no information.
-* The column separator must be `;`
-* **Don't** provide the title of columns
+
+* The column separator **must be** `;`
 
 ### Columns ###
-The number before the element indicates the column number.
 
-|reference (Compulsory)|title|artist_name|album_name|popularity|duration|isrc|year|tags|audio_path|audio_url|
+|reference|title|artist|audio_path|audio_url|album|popularity|duration|isrc|year|tags|
 |-|-|-|-|-|-|-|-|-|-|
-|string|string (Compulsory)|string|string|string|string|string|string|format should be: 'TagCollectionName&#124;TagName,TagCollectionName&#124;TagName,...'|Relative or absolute os path. Mandatory if no 'audio_url'|Absolute web url, Compulsory if no 'audio_path' (if both are provided, 'audio_url' used)|
+|string*|string*|string*|Relative or absolute os path. Mandatory if no 'audio_url'|Absolute web url, Mandatory if no 'audio_path' (if both are provided, 'audio_path' is used)|string|float|float|string|int|format should be: TagCollectionName&#124;TagName,TagCollectionName&#124;TagName,...|
 
-Compulsory elements:
-* 1 - title
-* 2 - artist_name
-* 9 - audio_path (relative or absolute os path)
-* 10 - audio_url (absolute web url)
+Mandatory elements (*):
+* reference
+* title
+* artist_name
 
-  You don't have to fill in both audio\_path AND audio\_url (if you do, the audio\_url will be used).
+You need to provide one of these elements:
+* audio_path (relative or absolute os path)
+* audio_url (absolute web url)
 
-Optional elements:
-* 0 - reference
-* 3 - album_name
-* 4 - popularity
-* 5 - duration
-* 6 - isrc
-* 7 - year
-* 8 - tags (format: `TagCollectionName|TagName,TagCollectionName|TagName,...`)
+  You don't have to fill in both audio_path AND audio_url (if you do, the audio_path will be used).
+
+All other elements are optional
+
+For the element "tags":
+* You must group similar tags under a TagCollection (Instruments, tempos, Genres, etc...)
+* You can then add as many tags as you want for each track following this format:   `TagCollectionName|TagName,TagCollectionName|TagName,...`
--- niland_importer.py
+++ niland_importer.py
@@ -5,151 +5,59 @@ from nilandapi.client import Client
 import urllib
 import csv
 import argparse
-from joblib import Parallel, delayed
-
-headers = {
-    'reference': 0,
-    'title': 1,
-    'artist_name': 2,
-    'album_name': 3,
-    'popularity': 4,
-    'duration': 5,
-    'isrc': 6,
-    'year': 7,
-    'tags': 8,
-    'audio_path': 9,
-    'audio_url': 10
-}
-
-
-def process_one(row):
-    try:
-        if len(headers) != len(row):
-            raise Exception('Malformed row. Expected %s field, having %s.' % (len(headers), len(row)))
-        tag_ids = importer._handle_tags(row['tags'])
-        try:
-            track = {
-                'reference': row['reference'],
-                'title': row['title'].title(),
-                'artist': row['artist_name'],
-                'album': row['album_name'],
-                'popularity': row['popularity'],
-                'duration': row['duration'],
-                'isrc': row['isrc'],
-                'year': row['year'],
-                'tags': tag_ids
-            }
-            requ = 'tracks/reference/%s' % row['reference']
-            print requ
-            existing_track = importer._client.get(requ)
-            print 'Reference %s already saved.' % (row['reference'])
-            change, track = preprocess_patch(existing_track, track)
-            if change:
-                print "Patching"
-                track = importer._client.patch('tracks/%d' % existing_track['id'], track)
-            else:
-                print "Passing"
-        except KeyboardInterrupt:
-            raise KeyboardInterrupt()
-        except:
-            if 0 != len(row['audio_path']):
-                track['audio'] = open(row['audio_path'], 'rb')
-            if 0 != len(row['audio_url']):
-                track['audio'] = open(importer._download_file(row['audio_url']), 'rb')
-            track = importer._client.post('tracks', track)
-            print 'Reference %s saved Niland ID %s' % (row['reference'], track['id'])
-    except Exception as e:
-        print 'Error for "%s": %s' % (row['reference'], e)
-        with open("errored.log", 'a') as f:
-            f.write("%s\t%s\n" % (e, row['reference']))
-
-
-def preprocess_patch(existing_track, new_track):
-    track_to_patch = dict()
-    change = False
-    for header in new_track.keys():
-        if header == "tags":
-            track_to_patch[header] = list(set(new_track['tags'] + [tag['id'] for tag in existing_track[header]]))
-            for el in track_to_patch[header]:
-                if el not in [tag['id'] for tag in existing_track[header]]:
-                    change = True
-        elif header == "artist":
-            if new_track["artist"]:
-                if not existing_track["artist"] or "name" not in existing_track["artist"] or existing_track["artist"]["name"] != new_track["artist"]:
-                    track_to_patch["artist"] = new_track["artist"]
-                    change = True
-            elif existing_track["artist"] and "name" in existing_track["artist"] and existing_track["artist"]["name"] == new_track["artist"]:
-                track_to_patch["artist"] = existing_track["artist"]["name"]
-            else:
-                track_to_patch["artist"] = ""
-        elif header == "album":
-            if new_track["album"]:
-                if not existing_track["album"] or "name" not in existing_track["album"] or existing_track["album"]["name"] != new_track["album"]:
-                    track_to_patch["album"] = new_track["album"]
-                    change = True
-            elif existing_track["album"] and "name" in existing_track["album"] and existing_track["album"]["name"] == new_track["album"]:
-                track_to_patch["album"] = existing_track["album"]["name"]
-            else:
-                track_to_patch["album"] = ""
-        else:
-            if not new_track[header] or str(existing_track[header]) == new_track[header]:
-                track_to_patch[header] = existing_track[header]
-            else:
-                track_to_patch[header] = new_track[header]
-                change = True
-    return change, track_to_patch
 
 
 class NilandImporter(object):
     def __init__(self, api_key, csv_path, start_line=0):
         self._client = Client(api_key)
-        if not start_line:
-            self._reader = csv.DictReader(open(csv_path, 'rb'), delimiter=';')
-            _ = self._reader.fieldnames
-        else:
-            self._reader = csv.DictReader(open(csv_path, 'rb'), delimiter=';')
-            for k in range(start_line):
-                next(self._reader)
         self._tag_collections = dict()
+        self._reader = csv.DictReader(open(csv_path, 'rb'), delimiter=';')
+        self._headers = self._reader.fieldnames
+        self._mandatory_headers = ['title', 'artist', 'reference']
+        for header in self._mandatory_headers:
+            if header not in self._headers:
+                raise NameError('%s is a mandatory column' % header)
 
-    def process(self, njobs):
-        Parallel(n_jobs=njobs, verbose=11)(delayed(process_one)(row) for row in self._reader)
+    def process(self):
+        normalized_headers = ['reference',
+                              'title',
+                              'artist',
+                              'album',
+                              'popularity',
+                              'duration',
+                              'isrc',
+                              'year',
+                              'tags',
+                              'audio_path',
+                              'audio_url']
+        for row in self._reader:
+            try:
+                track = dict()
+                for header in self._headers:
+                    if header in normalized_headers:
+                        if header == 'tags':
+                            tag_ids = self._handle_tags(row['tags'])
+                            track['tags'] = tag_ids
+                        elif 'audio' in header:
+                            pass
+                        else:
+                            track[header] = row[header]
+                    else:
+                        raise NameError('%s column name is unknown' % header)
 
-    def process_errored(self, njobs):
-        with open("errored.log", 'r') as f:
-            rows = f.read().split('\n')
-        open('errored.log', 'w').close()
-        references_to_process = [row.split('\t')[-1] for row in rows]
-        references_to_process = set(references_to_process)
-        rows_to_process = []
-        for k, row in enumerate(self._reader):
-            # print row['reference']
-            for reference in references_to_process:
-                if row['reference'] == reference:
-                    rows_to_process += [row]
-        Parallel(n_jobs=njobs, verbose=11)(delayed(process_one)(row) for row in rows_to_process)
-
-    def process_new_tracks(self, njobs):
-        page = 1
-        page_count = 1
-        all_existing_tracks = []
-        while page <= page_count:
-            print page
-            response = self._client.get('tracks', {'page': page, 'page_size': 1000})
-            all_existing_tracks += response['data']
-            page = response['page'] + 1
-            page_count = response['page_count']
-        all_existing_references = [track['reference'] for track in all_existing_tracks]
-        rows_to_process = []
-        for k, row in enumerate(self._reader):
-            process_this_one = True
-            for reference in all_existing_references:
-                if row['reference'] == reference:
-                    process_this_one = False
-            if process_this_one:
-                rows_to_process += [row]
-        Parallel(n_jobs=njobs, verbose=11)(delayed(process_one)(row) for row in rows_to_process)
-
+                if 'audio_path' in self._headers and 0 != len(row['audio_path']):
+                    track['audio'] = open(row['audio_path'], 'rb')
+                elif 'audio_url' in self._headers and 0 != len(row['audio_url']):
+                    track['audio'] = open(self._download_file(row['audio_url']), 'rb')
+                else:
+                    raise ValueError('You must provide an audio_path or an audio_url')
+                track = self._client.post('tracks', track)
+                print 'Reference %s saved Niland ID %s' \
+                    % (row['reference'], track['id'])
+            except Exception as e:
+                print 'Error for "%s": %s' % (row['reference'], e)
+                with open("errored.log", 'a') as f:
+                    f.write("%s\t%s\n" % (e, row['reference']))
 
     def _handle_tags(self, value):
         tag_ids = []
@@ -207,17 +115,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Niland Catalog Importer')
     parser.add_argument('--api-key', help='Your API key', required=True)
     parser.add_argument('--csv-path', help='Full path to the csv file', required=True)
-    parser.add_argument('--njobs', help='Number of parallel jobs to throw', required=False, default=1)
+    parser.add_argument('--njobs', help='Number of jobs to throw', required=True)
     parser.add_argument('--start-line', help='Line from which to start uploading', required=False, type=int)
-    parser.add_argument('--errored', help='Process only references listed in errored.log (This will erase the content of errored.log)', required=False, action="store_true")
-    parser.add_argument('--new-tracks', help='Process only new-tracks (no PATCHs only POSTs)', required=False, action="store_true")
     args = parser.parse_args()
 
     global importer
     importer = NilandImporter(args.api_key, args.csv_path, args.start_line)
-    if args.errored:
-        importer.process_errored(int(args.njobs))
-    elif args.new_tracks:
-        importer.process_new_tracks(int(args.njobs))
-    else:
-        importer.process(int(args.njobs))
+    importer.process(int(args.njobs))
